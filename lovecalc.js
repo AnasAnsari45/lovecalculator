728f4a2d98c3041fe4fe1bf682acc1f348dc0e3b
Anas Ansari <116506945+AnasAnsari45@users.noreply.github.com>
2024-03-06 01:09:23 +0500

    Add files via upload

---
+++ lovecalc.js
@@ -0,0 +1,8 @@
+//run this code on Chrome developmer mode (Ctrl + Shift + J)
+
+prompt('What is your name?')
+prompt('What is their name?')
+
+var a = Math.random();
+a = Math.floor(a*100) + 1;
+alert('Your love score is ' + a + '%')
